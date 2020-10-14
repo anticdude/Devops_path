78e962d8635f5ebe43776319d305d3e76e99404e
root <root@nginx.wc054n5p2dbuxotf20su0vx0mf.bx.internal.cloudapp.net>
2020-10-14 17:42:00 +0000

    add time function

--- Step1_learn_Golang/4_take_value_from_user.go
+++ Step1_learn_Golang/4_take_value_from_user.go
@@ -1,9 +1,14 @@
 package main
-import "fmt"
+import (
+"fmt"
+"time"
+)
 func main() {
 	fmt.Print("enter your money you want to double ;) ")
 	var input float64
 	fmt.Scanf("%f", &input)
 	var output = input * 2
-	fmt.Println("21 din me paisa double : ",output)
+	fmt.Println("21 din me paisa double : ",output) 
+	var time string = time.Now().Format(time.Stamp)
+	fmt.Println("Lakhpati banne ka time is ",time)
 }
