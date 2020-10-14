ebbc9ba04a966602c7eccc184264d986b9390802
Ankit Patel <65964218+anticdude@users.noreply.github.com>
2020-10-14 22:38:58 +0530

    Update 3_multiple_variable.go

--- Step1_learn_Golang/3_multiple_variable.go
+++ Step1_learn_Golang/3_multiple_variable.go
@@ -9,15 +9,10 @@ func main() {
 		e = a - b
 		f = d / e
 	)
-	fmt.Println("A is = ",a)
-	fmt.Println("A is = ",b)
-	fmt.Println("A is = ",c)
-	fmt.Println("A is = ",d)
-	fmt.Println("A is = ",e)
-	fmt.Println("A is = ",f)
-//		"b is = "b
-//		"c is = "c
-//		"* of c and a is = "d
-//		"- of a and b is = "e
-//		"/ of d and e is = "f )
+	fmt.Println("value of a is = ",a)
+	fmt.Println("value of b is = ",b)
+	fmt.Println("value of c is = ",c)
+	fmt.Println("value of d is = ",d)
+	fmt.Println("value of e is = ",e)
+	fmt.Println("value of f is = ",f)
 }
