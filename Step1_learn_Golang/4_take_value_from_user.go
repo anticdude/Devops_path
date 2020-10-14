0178a3da4456269cf6f7e2a07e43d8628dfd218b
root <root@nginx.wc054n5p2dbuxotf20su0vx0mf.bx.internal.cloudapp.net>
2020-10-14 17:19:23 +0000

    Take input from user : Intrective input in golang

--- Step1_learn_Golang/4_take_value_from_user.go
+++ Step1_learn_Golang/4_take_value_from_user.go
@@ -1,1 +1,9 @@
-i
+package main
+import "fmt"
+func main() {
+	fmt.Print("enter your money you want to double ;) ")
+	var input float64
+	fmt.Scanf("%f", &input)
+	var output = input * 2
+	fmt.Println("21 din me paisa double : ",output)
+}
